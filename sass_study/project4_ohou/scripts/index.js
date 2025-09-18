6cbdaa7dd5522341616e68fff23d0034afea7ac8
hyobin9824 <hyobin9824@gmail.com>
2025-09-18 18:08:45 +0900

    ohou JS 1-8 완료

--- sass_study/project4_ohou/scripts/index.js
+++ sass_study/project4_ohou/scripts/index.js
@@ -58,3 +58,175 @@ thumnailPic[1].addEventListener('mouseover',()=>{
 localStorage.setItem('isLogin','true');
 
 // 2. 😀저장하기 버튼 클릭 시 로그인 유무에 따라 다른결과 실행
+const bookMark = document.querySelector('.right_icon .scrap')
+console.log(bookMark)
+let loginStatus = localStorage.getItem('isLogin') ;
+bookMark.addEventListener('click',()=>{
+    if(loginStatus == 'true'){
+        //로그인시 wish.html이동
+        location.href = './wish.html';
+    }else if(loginStatus == 'false'){
+        //로그아웃시 login.html이동
+        location.href = './login.html';}
+    })
+    /* function bookMarkFunc(){
+        //상태가져오기 변수 생성
+        if(loginStatus == 'true'){
+            //로그인시 wish.html이동
+            location.href = './wish.html';
+            }else if(loginStatus == 'false'){
+                //로그아웃시 login.html이동
+                location.href = './login.html';
+                }
+                } */
+               // 3. 리뷰 클릭 시 스크롤 이동
+const reviewBtn = document.querySelectorAll('.review_link')
+const reviewWrap = document.querySelector('.review_wrap')
+const productBtn = document.querySelector('.product_link')
+const productInfo = document.querySelector('main > .product_info')
+
+reviewBtn[0].addEventListener('click',(e)=>{e.preventDefault();contentsPstFunc(reviewWrap)})
+reviewBtn[1].addEventListener('click',(e)=>{e.preventDefault();contentsPstFunc(reviewWrap)})
+productBtn.addEventListener('click',(e)=>{e.preventDefault();contentsPstFunc(productInfo)})
+
+
+function contentsPstFunc(target){
+    return window.scrollTo(0, target.offsetTop)
+}
+/* function reviewBtnFunc(e){
+    console.log(e)
+    e.preventDefault()
+    window.scrollTo(0,reviewWrap.offsetTop)
+    } */
+/* reviewBtn[0].addEventListener('click',(e)=>{
+    e.preventDefault()
+    //스크롤 이동 window 함수 scrollTo(x,y)
+    console.log(reviewWrap.offsetTop)//리뷰위치 위쪽 좌표값 확인
+    window.scrollTo(0,reviewWrap.offsetTop)
+    })
+    // sticky bar 항목 클릭 시 위치 이동
+    reviewBtn[1].addEventListener('click',(e)=>{
+        e.preventDefault()
+        //스크롤 이동 window 함수 scrollTo(x,y)
+        window.scrollTo(0,reviewWrap.offsetTop)
+})
+console.log(productBtn,productInfo)
+productBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    //스크롤 이동 window 함수 scrollTo(x,y)
+    console.log(productInfo.offsetTop)
+    window.scrollTo(0,productInfo.offsetTop)
+    }) */
+
+// 4. 가격정보 (i) 클릭 시 정보 팝업 출력/숨기기
+const priceInfoIcon = document.querySelector('.price_info .info_icon')
+const priceInfo = document.querySelector('.price_info .info')
+console.log(priceInfoIcon,priceInfo)
+// (i) 클릭 시 정보 팝업 출력/숨기기
+// 초기 : 숨김(false)
+// 클릭 : 보이기(true) -> false->true->false...
+
+let inforBoolean = false;
+
+priceInfoIcon.addEventListener('click',()=>{
+    //infoBoolean의 상태를 클릭 할 때마다 값 반전
+    inforBoolean = !inforBoolean;
+    console.log(inforBoolean);
+    //return priceInfo.style.display = 'block';
+    if (inforBoolean){//()조건이 참일때 자동실행
+        priceInfo.style.display = 'block';
+    } else{
+        priceInfo.style.display = 'none';
+    }
+})
+
+// 5. 주문 목록 초기 숨기기
+// 6-1. 사이즈select 초기 비활성화
+// 6-2. (색상 선택 시 활성화)
+// 7-1. 색상->사이즈 모두 선택 시 (선택한 인덱스가 0이 아닌 것) - 콘솔 선택완료
+const orderList = document.querySelector('.order_list')
+const sizeSelect = document.querySelector('#size_select')
+const colorSelect = document.querySelector('#color_select')
+const totalPrice = document.querySelector('.num_price p')
+const totalOrderPrice = document.querySelector('.order_price p em')
+console.log (orderList,sizeSelect,colorSelect)
+
+orderList.style.display = 'none';
+// colorSelect.children[0].disabled = true;
+sizeSelect.disabled = true;
+
+colorSelect.addEventListener('change',()=>{
+    // sizeSelect.disabled = false;
+    // 색상 select에서 첫번째 value=none에 해당하는 색상을 제외한 나머지 옵션이 선택되었을 때 사이즈select 활성화하기
+    if(colorSelect.selectedIndex != 0){
+        sizeSelect.disabled = false;
+        //사이즈 이벤트 작성위치
+        sizeSelect.addEventListener('change',()=>{
+            if(sizeSelect.selectedIndex!=0){
+                console.log('선택완료')
+                orderList.style.display = 'block';
+                //선택옵션 출력
+                let orderColor = colorSelect.options[colorSelect.selectedIndex].text
+                let orderSize = sizeSelect.options[sizeSelect.selectedIndex].text
+                //let orderColorReplace = orderColor.replace(찾는값, 변경값)
+                //정규표현식 시작과 끝 표시 /검사내용/
+                //괄호찾기 \찾는문자 \(.*\)
+                //모든 내용.*
+                let orderColorReplace = orderColor.replace(/\(.*\)/, '')
+                orderList.children[0].children[0].textContent = orderColorReplace;
+                let orderSizeReplace = orderSize.replace(/\(.*\)/, '')
+                orderList.children[0].children[1].textContent = orderSizeReplace;
+                // 7-2. 주문목록 출력(선택한 값이 출력, 주문금액 변경)
+                totalPrice.textContent = `${productOptDB[0].price.toLocaleString('ko-kr')}원`
+                totalOrderPrice.textContent = `${productOptDB[0].price.toLocaleString('ko-kr')}`
+            }
+        })
+    } else {
+        sizeSelect.disabled = true;
+    }
+})
+
+// DB불러오기 테스트
+console.log(productOptDB[0].name)//몽셸 패딩 하네스 올인원
+console.log(productOptDB[0].color[0],productOptDB[0].color[1])//블랙, 라즈베리
+
+// DB -> HTML 적용
+// 1. JS에서 HTML 함수로 생성 createElement
+const colorOpt1 = document.createElement('option')
+const colorOpt2 = document.createElement('option')
+console.log(colorOpt1,colorOpt2)
+// 2. 위에서 생성한 함수에 DB 데이터 대입 innerHTML, textContent
+colorOpt1.textContent = `${productOptDB[0].color[0]} (${productOptDB[0].price}원)`
+colorOpt2.textContent = `${productOptDB[0].color[1]} (${productOptDB[0].price}원)`
+// 3. 위에서 만든 HTML을 실제 HTML의 마지막 자식 위치로 삽입
+colorSelect.appendChild(colorOpt1)
+colorSelect.appendChild(colorOpt2)
+// * 생성한 태그가 li면 ul/ol의 마지막 자식 위치로 삽입
+// * 생성한 태그가 option이면 select의 마지막 자식 위치로 삽입
+
+// 8. 주문목록 'x' 클릭 주문목록 삭제, 주문금액 초기화
+const closeBtn = document.querySelector('.order_list .close')
+console.log(closeBtn)
+closeBtn.addEventListener('click',function(){
+    this.parentNode.style.display = 'none';
+    orderNum.value = '1';
+    totalPrice.textContent = '0';
+    totalOrderPrice.textContent = '0';
+    colorSelect.selectedIndex = colorSelect.options[0]
+    sizeSelect.selectedIndex = sizeSelect.options[0]
+})
+
+// 9. 주문목록 + 클릭 시 재고수량까지 주문수량+주문금액 표시
+const minusBtn = document.querySelector('#minus_btn')
+const plusBtn = document.querySelector('#plus_btn')
+const orderNum = document.querySelector('#order_num')
+let count = 1;
+
+plusBtn.addEventListener('click',()=>{
+    //수량증가
+    orderNum.value = ++count;
+    //주문금액
+    let amountPrice = productOptDB[0].price*orderNum.value;
+    totalPrice.textContent = `${amountPrice.toLocaleString('ko-kr')}원`
+    totalOrderPrice.textContent = `${amountPrice.toLocaleString('ko-kr')}`
+})
