e772f786b2da72e3bdd9577219e5b5136db2dea9
hyobin9824 <hyobin9824@gmail.com>
2025-09-23 18:10:41 +0900

    Torriden JS ing no8

--- sass_study/project5_Torriden/scripts/index.js
+++ sass_study/project5_Torriden/scripts/index.js
@@ -25,8 +25,6 @@ const thumbnailImg = document.querySelectorAll('.thumbnails a')
 const thumbnailBar = document.querySelectorAll('a .bar')
 console.log(overviewImg, thumbnailImg, thumbnailBar)
 
-imgChangeFunc('0')
-
 thumbnailImg[0].addEventListener('click',imgChangeFunc('0'))
 thumbnailImg[1].addEventListener('click',imgChangeFunc('1'))
 thumbnailImg[2].addEventListener('click',imgChangeFunc('2'))
@@ -60,8 +58,62 @@ function resetBar(){
 const arrowLeft = document.querySelector('.overview .left_arrow')
 const arrowRight = document.querySelector('.overview .right_arrow')
 console.log(arrowLeft, arrowRight)
-arrowRight.addEventListener('click',()=>{
-    imgChangeFunc(`0`)
-    let count = 0;
-    imgChangeFunc(`++${count}`)
+
+// 나중에 해보기
+
+// 5. 수량버튼 플러스마이너스 클릭시 갯수변경
+const minusBtn = document.querySelector('.quantity_btn .minus')
+const plusBtn = document.querySelector('.quantity_btn .plus')
+const orderNum = document.querySelector('#order_num')
+let count = 1;
+orderNum.value = count;
+
+//console.log(minusBtn, plusBtn,quantityInput)
+minusBtn.addEventListener('click',()=>{
+    if(count>1){
+        count--;
+        orderNum.value = count;
+    }else{
+        alert('값은 1 이상이어야 합니다')
+    }
 })
+plusBtn.addEventListener('click',()=>{
+    count++;
+    orderNum.value = count;
+})
+
+// 6-1. 선택박스 마우스 클릭시 아이콘 변경
+// 6-2. 선택박스 마우스 클릭시 이전 선택버튼 아이콘 복구
+const purchaseBg = document.querySelectorAll('.purchase_dot')
+const purchaseBtn = document.querySelectorAll('.purchaseBtn')
+const subPrice = document.querySelector('.sub_price')
+console.log(purchaseBtn, purchaseBg)
+
+purchaseBtn[0].addEventListener('click',()=>{checkDotFunc(0)})
+purchaseBtn[1].addEventListener('click',()=>{checkDotFunc(1)})
+purchaseBtn[2].addEventListener('click',()=>{checkDotFunc(2)})
+purchaseBtn[3].addEventListener('click',()=>{checkDotFunc(3)})
+
+function resetDotFunc(){
+    purchaseBg[0].style.backgroundImage = 'url(./images/emptydot.png)' ;
+    purchaseBg[1].style.backgroundImage = 'url(./images/emptydot.png)' ;
+    purchaseBg[2].style.backgroundImage = 'url(./images/emptydot.png)' ;
+    purchaseBg[3].style.backgroundImage = 'url(./images/emptydot.png)' ;
+}
+function checkDotFunc(num){
+    resetDotFunc()
+    purchaseBg[num].style.backgroundImage = 'url(./images/checkdot.png)' ;
+    if (num == '1'){
+        subPrice.textContent = '$21.38 USD';
+    }else if(num =='2'){
+        subPrice.textContent = '$22.09 USD';
+    }else if(num == '3'){
+        subPrice.textContent = '$22.56 USD';
+    }
+}
+
+// 7. 선택박스 마우스 클릭시 가격변경
+// 6의 함수와 합침!
+
+// 8-1. 카트버튼 마우스 올릴시 텍스트 애니메이션
+// 8-2. 카트버튼 마우스 클릭시 카트수량증가
